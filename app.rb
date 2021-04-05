b584aefbc14571ee990eb7127d6f64008eb430cd
Jesse Wenger <wengerj@gmail.com>
2021-04-05 21:18:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
   end
   
   get "/square/:number" do
-    @number = params[:number]
-    "#{@number.to_i ** 2}"
+    @number = params[:number].to_i
+    "#{@number ** 2}"
   end
   
   get "/say/:number/:phrase" do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
   end
 
