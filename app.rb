fd9c9006cf9e1919cf43a910d05f952bd6a20df2
Jesse Wenger <wengerj@gmail.com>
2021-04-05 21:23:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,18 +6,18 @@ class App < Sinatra::Base
 
   get "/reversename/:name" do
     @name = params[:name]
-    @name.reverse
+    "#{@name.reverse}"
   end
   
   get "/square/:number" do
     @number = params[:number].to_i
-    @number ** 2
+    "#{@number ** 2}"
   end
   
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    "#{@phrase}"
+    "#{@number.times do @phrase}"
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
