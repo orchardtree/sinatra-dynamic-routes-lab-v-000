7b24eb57175d0dbdb3a8b9bc746782efe94c7228
Jesse Wenger <wengerj@gmail.com>
2021-04-05 21:38:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times do
-      "#{puts @phrase}"
+      "#{@phrase}"
+      binding.pry
     end
   end
 
