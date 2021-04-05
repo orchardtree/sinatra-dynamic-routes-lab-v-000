1d8c0434802c16de77dd73a7fd0a87631eaf39ab
Jesse Wenger <wengerj@gmail.com>
2021-04-05 21:19:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
+    
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
