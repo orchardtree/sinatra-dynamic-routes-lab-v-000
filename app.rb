d98398ff157ad126b4b9dd3efb2709d8d466e8a1
Jesse Wenger <wengerj@gmail.com>
2021-04-05 21:05:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class App < Sinatra::Base
     @operation = params[:operation].to_sym
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    calc = self.send @operation @number1, @number2
+    calc = self.send @operation, @number1, @number2
   end
   
 end
