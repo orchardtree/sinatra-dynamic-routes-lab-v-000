95e885fc119e5ff1b47323777f1dd2287798efab
Jesse Wenger <wengerj@gmail.com>
2021-04-05 20:44:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,9 +29,9 @@ class App < Sinatra::Base
   
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
-    @number2 = params[:number1].to_i
+    @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    @number1.send :operation
+    send :operation, @number1 @number2
   end
   
 end
